3c2f9fa7f56ab964fa4da1af862f2cd895ae75c5
commitsovercoffee <commitsovercoffee@gmail.com>
2025-09-16 01:07:32 +0530

    adds transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,9 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import { navigating } from '$app/state';
+	import { fly } from 'svelte/transition';
+	import { cubicIn, cubicOut } from 'svelte/easing';
 
 	let { children } = $props();
 </script>
@@ -14,13 +17,17 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="bg-bg text-fg selection:bg-fg selection:text-bg">
+<div class="transition-color bg-bg text-fg duration-300 selection:bg-fg selection:text-bg">
 	<Nav />
-	<div
-		class="mx-auto prose p-4 prose-hr:border-border {theme.prefers === 'dark' && 'prose-invert'}"
-	>
-		{@render children?.()}
-	</div>
+	{#if !navigating.to}
+		<div
+			in:fly={{ easing: cubicOut, y: 40, duration: 300, delay: 300 }}
+			out:fly={{ easing: cubicIn, y: -40, duration: 300 }}
+			class="mx-auto prose p-4 prose-hr:border-border {theme.prefers === 'dark' && 'prose-invert'}"
+		>
+			{@render children?.()}
+		</div>
+	{/if}
 	<Footer />
 </div>
 
